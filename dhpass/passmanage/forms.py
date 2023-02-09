690e8b4a59afa4348724c854f6fc76daa746a431
Dylan Harvey <dharveyasp@gmail.com>
2023-02-09 16:23:30 -0500

    Created forms and configured models

---
+++ dhpass/passmanage/forms.py
@@ -0,0 +1,77 @@
+from django import forms
+from .models import User
+non_allowed_usernames = ['abc']
+
+
+class RegisterForm(forms.Form):
+    username = forms.CharField()
+    email = forms.EmailField()
+    password1 = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(
+            attrs={
+                "id": "user-password"
+            }
+        )
+    )
+    password2 = forms.CharField(
+        label='Confirm Password',
+        widget=forms.PasswordInput(
+            attrs={
+                "id": "user-confirm-password"
+            }
+        )
+    )
+
+    # Check if passwords match
+    def check_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if not password2:
+            raise forms.ValidationError("Please retype your password")
+        if password1 != password2:
+            raise forms.ValidationError("Passwords do not match")
+        return password2
+
+    # Checks if inputted username matches existing User
+    def check_username(self):
+        username = self.cleaned_data.get("username")
+        qs = User.objects.filter(username__iexact=username)
+        if username in non_allowed_usernames:
+            raise forms.ValidationError("Invalid Username, please use another.")
+        if qs.exists():
+            raise forms.ValidationError("This username is already in use.")
+        return username
+
+    # Check if email is already in use
+    def check_email(self):
+        email = self.cleaned_data.get("username")
+        qs = User.objects.filter(email__iexact=email)
+        if qs.exists():
+            raise forms.ValidationError("This email is already in use.")
+        return email
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={
+            "class": "form-control"
+    }))
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "id": "user-password"
+            }
+        )
+    )
+
+    def check_username(self):
+        username = self.cleaned_data.get("username")
+        qs = User.objects.filter(username__iexact=username)  # gets inputted username and then checks if exists
+        if not qs.exists():
+            raise forms.ValidationError("This is an invalid user.")
+        if qs.count() != 1:
+            raise forms.ValidationError("This is an invalid user.")
+        return username
--- dhpass/passmanage/views.py
+++ dhpass/passmanage/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class Home(generic.TemplateView):
+    template_name = 'passmanage/home.html'
+
+
+class Login(generic.TemplateView):
+    template_name = 'passmanage/login.html'
+
+    def sign_in(self):
+        """Signin Function"""
+        return
+
+
+class Register(generic.TemplateView):
+    template_name = 'passmanage/register.html'
+
+
+class Passwords:
+    pass
+
+
+class AddPassword:
+    pass
+
+
+class PasswordDetails:
+    pass
